7a4ac430f775414350d36d811b5540ab2d3bde5c
samy gawande <samy@yahoo-inc.com>
2016-09-17 15:59:11 +0000

    getting hostlist and converting to newline and commas in a file

---
+++ todo/writehostlists.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import hostlists
+import argparse
+parser=argparse.ArgumentParser()
+parser.add_argument("--expand2comma")
+parser.add_argument("--expand2line")
+args=parser.parse_args()
+
+def converttocoma(hostlist):
+  fw=open("/tmp/hl-c","w")
+  hostlist=args.expand2comma
+  xl=[]
+  xl=(hostlists.expand(hostlist))
+  fw.write(', '.join(xl))
+  fw.close()
+
+def converttonewline(hostlist):
+  fw=open("/tmp/hl-l","w")
+  hostlist=args.expand2line
+  xl=[]
+  xl=(hostlists.expand(hostlist))
+  fw.write('\n'.join(xl))
+  print '\n'.join(xl)
+
+def main():
+  if args.expand2comma:
+    converttocoma(args.expand2comma)
+  elif args.expand2line:
+    converttonewline(args.expand2line)
+
+if __name__=="__main__":
+  main()
