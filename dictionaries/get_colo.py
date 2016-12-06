bb9453c2e03736e64c7cb6cf4e6b743553e43171
samy gawande <samy@yahoo-inc.com>
2016-12-06 21:53:31 +0000

    more

--- dictionaries/get_colo.py
+++ dictionaries/get_colo.py
@@ -5,7 +5,7 @@ f = open(sys.argv[1],"rU")
 for i in f:
     SPLIT = i.split('.')
 #    print SPLIT[-3]
-    colo =  SPLIT[-3]
+    colo =  SPLIT[-4]
     if colo not in hash:
         hash[colo] = 1
     else:
