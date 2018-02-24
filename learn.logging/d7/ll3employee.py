4b5295a380560cb821488c69ba4a26f8ef85b527
Sammer Gawande <sameergawande@yahoo.com>
2018-02-24 12:34:04 -0800

    sdds

--- learn.logging/d7/ll3employee.py
+++ learn.logging/d7/ll3employee.py
@@ -15,6 +15,6 @@ class Employee:
 		logger.debug("Employee with name {} {} and salary {} has been created".format(self.fname,self.lname,self.salary))
 
 emp1 = Employee("sameer","gawande",89000)
-emp1 = Employee("rakhee","gawande",19000)
-emp1 = Employee("puru","kumar",99000)
+emp2 = Employee("rakhee","gawande",19000)
+emp3 = Employee("puru","kumar",99000)
 		
