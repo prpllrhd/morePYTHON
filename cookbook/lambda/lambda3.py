a09517f4547c4dc3ac027eb05ffbeb56010e7a29
samy <samy@rakhees-Air.fios-router.home>
2018-07-04 17:04:27 -0400

    sdsd

--- cookbook/lambda/lambda3.py
+++ cookbook/lambda/lambda3.py
@@ -5,6 +5,13 @@ filter in lambda is used when u want to filter numbers using a condition like he
 """
 def script():
 	list_a = [1, 2, 3, 4, 5]
+	"""
+	2 ways to write it.
+	def f(x):
+		return x % 2 !=0 and x % 3 !=0
+	a = filter(f,range(100))
+	print list(a)
+	"""
 	filter_obj = filter(lambda x: x % 2 == 0 and x % 3 == 0, range(100)) # filter object <filter at 0x4e45890>
 	even_num = list(filter_obj) # Converts the filer obj to a list
 	print(even_num) # Output: [2, 4]
